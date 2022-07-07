6fda37fbebbdb0f2f6de20182b4efd4abb5d9b3e
qqqrqq <romamedv298@gmail.com>
2022-07-07 07:26:31 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference')
-  .version('0.8.0');
+  .version('0.8.0')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
